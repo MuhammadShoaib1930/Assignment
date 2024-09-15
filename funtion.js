45ad4319a30f7afb5e386ebb64fb0d3d8899d152
M.Shoaib <shoaibsn54321@gmail.com>
2024-09-15 10:24:57 +0500

    Assigment 3

---
+++ funtion.js
@@ -0,0 +1,11 @@
+
+function btnBlueColor() {
+    alert("Blue Color");
+    document.write("This is my class.");
+}
+function btnGreenColor() {
+    alert("Green Color");
+}
+function btnRedColor() {
+    alert("Red Color");
+}
